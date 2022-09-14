a77ede383758f93fc7716675971727b1753a21d6
ismamero05 <ismailovmuhammadyusuf3@gmail.com>
2022-09-15 00:15:13 +0500

    3-dars vazifasi

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,12 +1,58 @@
 import React, { Component } from 'react';
+import google from './assets/google.png'
+import facebook from './assets/facebook.png'
+import linkedin from './assets/linkedin.png'
 
 class Navbar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            // son: 0
+            title: ''
+        }
+    }
+
+    
     render(){
-        console.log(this);
+        // const plus = () => {
+            //     this.setState({son: this.state.son + 1})
+            // }
+            
+            // const minus = () => {
+                //     this.setState({son: this.state.son - 1})
+                // }
+                let getInp = (e) => {
+                    this.setState({title: e.target.value})
+                }
+                
         return (
-            <div className='box'>
-                <img className='imad' src={this.props.title.img} alt="" />
-                <h3>{this.props.title.des}</h3>
+            <div className='main'>
+                    {/* <button className='sec' onClick={minus}>-</button>
+                    <h1>{this.state.son}</h1>
+                    <button className='fir' onClick={plus}>+</button> */}
+
+
+                    <div className="fir-con">
+                        <h1 className='sign-up-let'>sign up</h1>
+                        <p className='email-let'>email</p>
+                        <input type="email" name="email" id="email-in" onChange={getInp} />
+                        <p className='pass-let'>password</p>
+                        <input type="password" name="password" id="pass-in" />
+                        <button className="sign-up">sign up</button>
+                        <div className="or-div">
+                            <div className="hr"></div>
+                            <div className="or-box">or</div>
+                            <div className="hr"></div>
+                        </div>
+                        <div className="images">
+                            <img src={google} alt="" />
+                            <img src={facebook} alt="" />
+                            <img src={linkedin} alt="" />
+                        </div>
+                        <p className='last'>Already a user? <a href="#">Login</a></p>
+                        <br />
+                        <h1>{this.state.title}</h1>
+                    </div>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,36 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Footer from './footer.jsx'
 import './index.css'
-import rasm from './assets/2-2-2-3foodgroups_fruits_detailfeature.jpg' 
 
-const data = [
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-  {img: rasm, des: 'rasm1'},
-]
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='con'>
-      {
-        data.map((value) =>{
-          return (
-            <Footer title={value} />
-          )
-          
-        })
-      }
-    </div>
+    <Footer />
   </React.StrictMode>
 );
 
